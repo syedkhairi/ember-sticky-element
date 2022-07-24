70bd6ed6cbf45977de635b6c471a99796017d98a
syed <syedkhairi@yahoo.com>
2022-07-24 23:47:46 +0100

    brocolli build options

--- index.js
+++ index.js
@@ -3,14 +3,18 @@
 module.exports = {
   name: require('./package').name,
 
-  included: function(app, parentAddon) {
-    var target = (parentAddon || app);
+  included(app, parentAddon) {
+    let target = (parentAddon || app);
 
     // necessary for nested usage
     if (target.app) {
       target = target.app;
     }
 
+    target.options = target.options || {};
+    target.options.babel = target.options.babel || { includePolyfill: true };
+
     target.import('vendor/ember-sticky-element.css');
+    return this._super.included.apply(this, arguments);
   }
 };
